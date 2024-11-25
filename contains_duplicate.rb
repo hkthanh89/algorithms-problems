8924217fade1bdd89b5ae80f7546a31fed8c157b
Thanh Huynh <hkthanh89@users.noreply.github.com>
2024-11-25 13:06:49 +0700

    solved Contains Duplicate (updated)

--- contains_duplicate.rb
+++ contains_duplicate.rb
@@ -13,6 +13,18 @@ def solutions(nums)
   return false
 end
 
+# @param {Integer[]} nums
+# @return {Boolean}
+def contains_duplicate(nums)
+  h = {}
+  nums.each do |num|
+    h[num] = (h[num] || 0) + 1
+    return true if h[num] == 2
+  end
+
+  return false
+end
+
 puts solutions([1,2,3,1])
 puts solutions([1,2,3,4])
 puts solutions([1,1,1,3,3,4,3,2,4,2])
